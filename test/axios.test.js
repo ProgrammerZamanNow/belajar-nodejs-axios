e692a4697608932b6f1e8fed491bf36571c268af
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-25 19:36:24 +0700

    Add HTTP Method tests to axios.test.js
    Enhanced the test coverage for axios.test.js by introducing new tests aimed at verifying the GET HTTP Method functionality. Tests ensure both standard use and use with query parameters and headers are properly supported.

--- test/axios.test.js
+++ test/axios.test.js
@@ -9,3 +9,29 @@ describe('HTTP Client', () => {
         expect(httpClient).toBeDefined();
     });
 });
+
+describe('HTTP Method', () => {
+    const httpClient = axios.create({
+        baseURL: "https://enbcvv013n034.x.pipedream.net",
+        timeout: 5000
+    });
+
+    it('should support GET method', async () => {
+        const response = await httpClient.get('/');
+        expect(response.status).toBe(200);
+    });
+
+    it('should support GET method with config', async () => {
+        const response = await httpClient.get('/', {
+            params: {
+                name: "Eko"
+            },
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+        expect(response.status).toBe(200);
+        expect(response.statusText).toBe("OK");
+        expect(response.data.success).toBe(true);
+    });
+});
